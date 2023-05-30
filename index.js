b6bba604e48bce0573c7998da8258352e55fff9b
nachoprado86 <fonsvitae.spain@gmail.com>
2023-05-30 17:47:44 +0200

    casi terminado

--- index.js
+++ index.js
@@ -1,2 +1,84 @@
 // NACHO PRADO
 
+// 1
+let Coche = {
+  marca: "seat",
+  modelo: "panda",
+  matricula: 4848,
+};
+// 2
+let Casa = {
+  codPostal: 41980,
+  calle: "alfajores",
+  portal: 8,
+  piso: 2,
+};
+// 3
+let FullStackDeveloper = {
+  lenguajes: [],
+  proyectos: [],
+};
+// 4
+let Perro = {
+  nombre: "",
+  raza: "",
+  color: "",
+  edad: "",
+  ladrar: function () {
+    console.log("guau");
+  },
+  popo: function () {
+    return Math.random() * 3;
+  },
+};
+// 5
+let marcaPortatil = Portatil.marca;
+// 6
+let marcaPortatil2 = Portatil["marca"];
+// 7
+let grupos = Concierto.grupos;
+// 8
+let RGB = Led[(Rojo, Verde, Azul)];
+// 9
+Portatil.modelo = "P345";
+// 10
+// Concierto.cartelera = "Guns N' Roses";
+// 11
+Concierto.fecha = new Date(30 / 05 / 2023);
+// 12
+// Impresora.imprimiendo = objeto con propiedades: nombreArchivo, copias, numPaginas
+// 13
+let Noticia = {
+  titular: "paquito",
+  cuerpo: "negrita",
+};
+// 14
+let Persona = {
+  nombre: "raul",
+  apellidos: "fernandez",
+  edad: 18,
+};
+// 15
+const Avion = {
+  numPasajeros: 20,
+  despegar: function () {
+    console.log("despegando");
+  },
+  volar: function () {
+    console.log("llegando al destino");
+  },
+  aterrizar: function () {
+    console.log("aterrizando");
+  },
+};
+// 16
+// 17
+// 18
+// 19
+// 20
+// 21
+// 22
+// 23
+// 24
+// 25
+// 26
